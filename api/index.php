15af93a0f12a97e1bf02593eb733e3d7de175aac
Bataino <bataino.ronaldo@gmail.com>
2024-06-08 12:49:03 +0100

    ob_flush issue

--- api/index.php
+++ api/index.php
@@ -7,9 +7,9 @@ MSSN Title
 <body>
 <?php
 
-// error_reporting(1);
+error_reporting(0);
 // error_reporting(E_ERROR | E_PARSE);
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); 
+// ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); 
 
 ob_start();
 require_once __DIR__ . '/../vendor/autoload.php';
