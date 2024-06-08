dda9274c4e84b9da94e9ef5fa190eb5a20c468e5
Bataino <bataino.ronaldo@gmail.com>
2024-06-08 11:29:04 +0100

    wrong vercel path

--- api/index.php
+++ api/index.php
@@ -12,7 +12,7 @@ MSSN Title
 ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); 
 
 // ob_start();
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 use setasign\Fpdi\Fpdi;
 
 
